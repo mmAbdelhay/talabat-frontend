377031490ab459d2afb486ecb1e123494900c4db
karim-arafa <karimahmedarafa@gmail.com>
2021-06-03 23:03:56 +0200

    retriece nearest providers

--- src/views/providers/nearestProviders/AllNearestProviders.js
+++ src/views/providers/nearestProviders/AllNearestProviders.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import "antd/dist/antd.css";
 import Item from 'antd/lib/list/Item';
 import RestaurantComponent from '../restaurantComponent';
+import { message } from 'antd';
 
 class AllNearestProviders extends React.Component{
 
@@ -10,19 +11,21 @@ class AllNearestProviders extends React.Component{
       super(props);
       this.state={
         restaurants:[],
-        resLat : sessionStorage.getItem('nearlat'),
-        resLng: sessionStorage.getItem('nearlng') 
+        resLat : 31.106267,
+        resLng : 29.756120
+        // resLat : sessionStorage.getItem('nearlat'),
+        // resLng: sessionStorage.getItem('nearlng') 
       };
   }
 
   async componentDidMount() {
-    const response = await axios.post("http://localhost:5000/api/v1/client/lookup/nearproviders", {latitude: this.state.resLat,
+    const response = await axios.post("http://localhost:5000/api/v1/guest/lookup/nearproviders", {latitude: this.state.resLat,
     longitude: this.state.resLng});
-   console.log(response)
+   //console.log(response)
    console.log("----------------------");
-   console.log(response.data);
-   console.log(typeof response.data.All_Restaurants);
-    this.setState({restaurants:response.data.All_Restaurants})
+   console.log(response.data.Message);
+   //console.log(typeof response.data.All_Restaurants);
+    this.setState({restaurants:response.data.Message})
   }
 
 
@@ -32,7 +35,7 @@ class AllNearestProviders extends React.Component{
         <div className="container border pt-2 shadow p-3 mb-5 bg-white rounded">
           <h3 className="bg-light pb-1 pt-1 mb-5">All Restaurants</h3>
         <div className="row">
-        {this.state.restaurants.map((item) => {
+        {this.state.restaurants?.map((item) => {
                   return (
                         <RestaurantComponent key={item.id} id={item.id} name={item.name} logo={item.logo} type={item.provider_type} />
                   );
