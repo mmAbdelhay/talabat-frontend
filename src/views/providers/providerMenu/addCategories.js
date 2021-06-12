1fb484ef97cc592c03e8b591196f1052d6cc9efc
ali-khaled-ali <alikhaledali1010@gmail.com>
2021-06-12 04:38:11 +0200

    fixed form page layout

--- src/views/providers/providerMenu/addCategories.js
+++ src/views/providers/providerMenu/addCategories.js
@@ -47,31 +47,50 @@ class AddCategory extends React.Component{
       };
 
        return (
-    <Form
-      {...layout}
-      name="basic"
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="Category name"
-        name="name"
-        rules={[
-          {
-            required: true,
-            message: 'Please input category name',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <div className="container">
+          <div className="row">
+            <div className="col"></div>
+  
+  
+  
+            <div className="col-10 bg-success shadow-sm p-3 mb-5 bg-white ">
+                <h2>Add Category</h2><hr/>
+      
+
+                    <Form
+                      {...layout}
+                      name="basic"
+                      onFinish={onFinish}
+                      onFinishFailed={onFinishFailed}
+                    >
+                      <Form.Item
+                        label="Category name"
+                        name="name"
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please input category name',
+                          },
+                        ]}
+                      >
+                        <Input />
+                      </Form.Item>
+
+                      <Form.Item {...tailLayout}>
+                        <Button type="primary" htmlType="submit">
+                          Submit
+                        </Button>
+                      </Form.Item>
+                    </Form>
+
+                </div>
+
+
+              <div className="col"></div>
+
+            </div>
+          </div>
   );
   }
 }
