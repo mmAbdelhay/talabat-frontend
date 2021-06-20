282707a23161f889608626ee00ca8fa5faed2a6e
ali-khaled-ali <alikhaledali1010@gmail.com>
2021-06-20 13:13:17 +0200

    handled server-down in all restaurants

--- src/views/providers/allRestaurants.js
+++ src/views/providers/allRestaurants.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from "axios";
 import RestaurantComponent from './restaurantComponent'
 import { ServerIP } from "../../assets/config";
+import ErrorPage from "../sharedComponents/ErrorPages/ErrorPage";
 
 class AllResturants extends React.Component{
 
@@ -13,13 +14,33 @@ class AllResturants extends React.Component{
         searchstring:"",
         currentPage: 1,
         postsPerPage:3,
+        error:"",
       };
 
   }
 
   async componentDidMount() {
-    const response = await axios.get(`${ServerIP}/api/v1/guest/restaurants/all`, {});
-    this.setState({restaurants:response.data.All_Restaurants,searchedRests:response.data.All_Restaurants})
+    const response = await axios.get(`${ServerIP}/api/v1/guest/restaurants/all`,
+    
+    {}).then((res) => {
+     
+
+      console.log(res)
+
+      
+    this.setState({restaurants:res.data.All_Restaurants,searchedRests:res.data.All_Restaurants})
+    console.log('restranusts' ,this.state.restaurants)
+    })
+    .catch((err) => {
+      console.log(err)
+      if (err.response)
+        this.setState({error: err.response.status})
+      else 
+      this.setState({error: 500})    
+    });
+    
+    ;
+    
   }
 
   handleSearchChange=(e)=>{
@@ -61,7 +82,10 @@ class AllResturants extends React.Component{
     let indexOfLastPost = this.state.currentPage * this.state.postsPerPage;
     let indexOfFirstPost = indexOfLastPost - this.state.postsPerPage;
     let curentPosts = this.state.searchedRests.slice(indexOfFirstPost, indexOfLastPost);
-       return (
+       
+    if(this.state.restaurants.length>0){
+      console.log('hellooo',this.state.restaurants)
+    return (
         <div className="container border pt-2 shadow p-3 mb-5 bg-white rounded">
           <h3 className="pb-1 pt-1 mb-5" style={{backgroundColor:"#F5F5F5"}}>All Restaurants</h3>
           {this.state.restaurants.length?<div className="input-group mb-3" style={{width:"30%"}}>
@@ -88,7 +112,17 @@ class AllResturants extends React.Component{
           </ul>
         </nav>:""}</div>:<h1>No Restaurants Available</h1>}
         </div>
-       );
+       )
+      };
+      console.log('not hello',this.state.error)
+      if(this.state.error){
+        console.log('not hello')
+
+        return( <ErrorPage err={`${this.state.error}`} />)
+
+      }
+
+      return false
   }
 }
 
